40cdc7fd5eda5f9cafd5441235e5c9de34b815d4
dgnajera <dgnajera2@miners.utep.edu>
2016-02-28 15:55:40 -0700

    Create index.php

---
+++ play/index.php
@@ -0,0 +1,11 @@
+<?php
+	include 'playChecker.php';
+	// include 'Board.php';
+
+	// $board = new Board(15);
+	// $board->placeStone($x,$y);
+	// echo "I have placed a stone at $x,$y";
+	// echo "\n";
+	// echo $board->at($x,$y)->hasStone();
+	
+?>
